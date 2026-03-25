d46b84f731ca428d08f344f6878e14c7b3e9a40a
Matthew Swaney <Matthew.J.Swaney@gmail.com>
2026-03-25 14:27:47 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import LoginSignup from './components/assets/login_signup/LoginSignup';
 
 function App() {
   return (
     <div>
-
+      <LoginSignup/>
     </div>
   );
 }
---
+++ src/components/assets/login_signup/LoginSignup.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./LoginSignup.css";
+
+import user_icon from '../person.png'
+import email_icon from '../email.png'
+import password_icon from '../password.png'
+
+const LoginSignup = () => {
+    return (
+        <div className = 'container'>
+
+            <div classname = 'header'>
+                <div className="text">SignUp</div>
+                <div className="underline"></div>
+            </div>
+            <div className="inputs">
+            <div className="input">
+                <img src={user_icon} alt="" />
+                <input type="text" />
+            </div>
+            <div className="input">
+                <img src={email_icon} alt="" />
+                <input type="email" />
+            </div>
+            <div className="input">
+                <img src={password_icon} alt="" />
+                <input type="password" />
+            </div>
+            </div>
+
+        </div>
+    );
+}
+
+export default LoginSignup;
