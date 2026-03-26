949a6a2b1ea4dba83eb1c9c8c5de723e66139d8f
Matthew Swaney <Matthew.J.Swaney@gmail.com>
2026-03-25 19:44:22 -0500

    update

--- src/components/assets/login_signup/LoginSignup.jsx
+++ src/components/assets/login_signup/LoginSignup.jsx
@@ -9,23 +9,32 @@ const LoginSignup = () => {
     return (
         <div className = 'container'>
 
-            <div classname = 'header'>
-                <div className="text">SignUp</div>
+            <div className = 'header'>
+                <div className="text">Sign Up</div>
                 <div className="underline"></div>
             </div>
+
             <div className="inputs">
             <div className="input">
                 <img src={user_icon} alt="" />
-                <input type="text" />
+                <input type="text" placeholder="Name"/>
             </div>
+
             <div className="input">
                 <img src={email_icon} alt="" />
-                <input type="email" />
+                <input type="email" placeholder="Email ID"/>
             </div>
+
             <div className="input">
                 <img src={password_icon} alt="" />
-                <input type="password" />
+                <input type="password" placeholder="Password"/>
             </div>
+            </div>
+
+            <div className="forgot-password">Lost Password? <span>Click Here!</span></div>
+            <div className="submit-container">
+                <div className="submit">Sign Up</div>
+                <div className="submit">Login</div>
             </div>
 
         </div>
